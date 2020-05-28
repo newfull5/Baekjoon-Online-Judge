56d8c9fc42fd1795ab46df0b5880c9ed5e657420
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-05-28 20:11:07 +0900

    Create [11047] 동전 0.py

---
+++ [11047] 동전 0.py
@@ -0,0 +1,14 @@
+N, K = list(map(int, input().split()))
+cost = []
+for _ in range(N):
+    temp = int(input())
+    if temp > K:
+        break
+    cost.append(temp)
+    
+answer = 0
+for i in range(len(cost)-1,-1,-1):
+    temp, K = divmod(K, cost[i])
+    answer += temp
+    
+print(answer)
