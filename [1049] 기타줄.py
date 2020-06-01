febfffa43dfef95e4c22a6a82c6a3fae7a85b7c1
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-06-01 09:07:22 +0900

    Create [1049] 기타줄.py

---
+++ [1049] 기타줄.py
@@ -0,0 +1,17 @@
+import math
+
+a, b = map(int, input().split())
+
+pack, rest = float('inf'), float('inf')
+
+for _ in range(b):
+    temp1, temp2 = map(int, input().split())
+    
+    if temp1 < pack:
+        pack = temp1
+        
+    if temp2 < rest:
+        rest = temp2
+        
+        
+print(min(rest*a, pack*math.ceil(a/6), (pack*divmod(a,6)[0]) + (rest*divmod(a,6)[1])))
