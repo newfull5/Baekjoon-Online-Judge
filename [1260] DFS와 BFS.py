f9499eb4ff4f3051282056d34e6968343bf55c88
Sae_Chan_Oh <dhtocks@naver.com>
2020-06-22 14:41:56 +0900

    Update [1260] DFS와 BFS.py

--- [1260] DFS와 BFS.py
+++ [1260] DFS와 BFS.py
@@ -1,4 +1,4 @@
-n,m,v = map(int, input().split())
+'''n,m,v = map(int, input().split())
 matrix = [[0] * (n + 1) for _ in range(n + 1)]
 visit = [0 for i in range(n + 1)]
 
@@ -29,3 +29,34 @@ def dfs(start, visited):
 
 print(*dfs(v,[]))
 print(*bfs(v))
+'''
+N, M, V = map(int, input().split())
+s = [[0]*(N+1) for _ in range(N+1)]
+for _ in range(M):
+    x,y = map(int, input().split())
+    s[x][y] = 1
+    s[y][x] = 1
+    
+def bfs(current):
+    visit = [current]
+    queue = [current]
+    
+    while queue:
+        n = queue.pop(0)
+        for i in range(N+1):
+            if s[n][i] == 1 and (i not in visit):
+                visit.append(i)
+                queue.append(i)
+    return visit
+
+def dfs(current,visit):
+    visit.append(current)
+    
+    for i in range(N+1):
+        if s[current][i] == 1 and (i not in visit):
+            dfs(i, visit)
+    
+    return visit
+
+print(*dfs(V,[]))
+print(*bfs(V))
