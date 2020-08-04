dbd6b7d3e9b64f6f0c4c75c129e519942a85e543
Sae_Chan_Oh <dhtocks@naver.com>
2020-08-04 10:42:16 +0900

    Create [2941] 크로아티아 알파벳.py

---
+++ [2941] 크로아티아 알파벳.py
@@ -0,0 +1,12 @@
+string = input()
+
+string = string.replace('c=','A')
+string = string.replace('c-','B')
+string = string.replace('dz=','C')
+string = string.replace('d-','D')
+string = string.replace('nj','E')
+string = string.replace('lj','F')
+string = string.replace('s=','G')
+string = string.replace('z=','H')
+
+print(len(string))
