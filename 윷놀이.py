bfe11948ad825749e38d9a4cefc9569da54f4772
Sae_Chan_Oh <dhtocks@naver.com>
2020-10-18 09:44:38 +0900

    Create 윷놀이.py

---
+++ 윷놀이.py
@@ -0,0 +1,13 @@
+for _ in range(3):
+    a = input().count('0')
+
+    if a == 1:
+        print('A')
+    if a == 2:
+        print('B')
+    if a == 3:
+        print('C')
+    if a == 4:
+        print('D')
+    if a == 0:
+        print('E')
