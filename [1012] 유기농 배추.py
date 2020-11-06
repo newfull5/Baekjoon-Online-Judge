f67c3bd219056615da4ad46b3bb6b3c6956c1f83
Sae_Chan_Oh <dhtocks@naver.com>
2020-11-06 19:24:45 +0900

    Update [1012] 유기농 배추.py

--- [1012] 유기농 배추.py
+++ [1012] 유기농 배추.py
@@ -1,3 +1,4 @@
+"""
 T = int(input())
 
 for _ in range(T):
@@ -37,3 +38,51 @@ for _ in range(T):
                 cnt -= 1
 
     print(abs(cnt+1))
+"""
+#2020.11.06
+import sys
+sys.setrecursionlimit(10 ** 4)
+input = sys.stdin.readline
+
+def DFS(cnt,a,b):
+    
+    s[a][b] = cnt
+    
+    if a != (n-1):
+        if s[a+1][b] == 1: #아래로 이동
+            DFS(cnt, a+1, b)
+    
+    if a != 0:
+        if s[a-1][b] == 1: #위로 이동
+            DFS(cnt, a-1, b)
+        
+    if b != (m-1):
+        if s[a][b+1] == 1: #오른쪽 이동
+            DFS(cnt, a, b+1)
+        
+    if b != 0:
+        if s[a][b-1] == 1: #왼쪽 이동
+            DFS(cnt, a , b-1)
+
+T = int(input())
+
+for _ in range(T):
+    
+    m,n,k = map(int, input().split())
+
+    s = [[0]*m for _ in range(n)]
+
+    for _ in range(k):
+        a,b = map(int, input().split())
+        s[b][a] = 1
+
+    cnt = 2
+    
+    for i in range(n):
+        for j in range(m):
+
+            if s[i][j] == 1:
+                DFS(cnt,i,j)
+                cnt += 1
+                
+    print(cnt-2)
