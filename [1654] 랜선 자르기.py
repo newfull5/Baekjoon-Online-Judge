8cad9db726e2b2a5e99613d2b867febdb570b2f2
Sae_Chan_Oh <dhtocks@naver.com>
2020-12-11 13:18:15 +0900

    Update [1654] 랜선 자르기.py

--- [1654] 랜선 자르기.py
+++ [1654] 랜선 자르기.py
@@ -1,3 +1,5 @@
+"""
+#2020.07.29
 a, b = map(int, input().split())
 
 stack = []
@@ -14,5 +16,28 @@ while left<=right:
         left= mid+1
     elif temp < b:
         right = mid-1
+        
+print(right)
+"""
+#2020.12.11
+a,b = map(int, input().split())
+
+arr = []
+for _ in range(a):
+    arr.append(int(input()))
+
+
+left, right = 1, max(arr)
+
+while left<=right:
+    
+    mid = (left + right)//2
+    
+    answer = sum(map(lambda x: x//mid, arr))
+    
+    if answer >= b:
+        left = mid + 1
+    else:
+        right = mid -1
         
 print(right)
