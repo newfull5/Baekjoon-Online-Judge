a81a2aa8293cb76d76ebebea8c035dd0707bd66a
Sae_Chan_Oh <dhtocks@naver.com>
2020-12-14 16:17:23 +0900

    Update [10870] 피보나치 수 5.py

--- [10870] 피보나치 수 5.py
+++ [10870] 피보나치 수 5.py
@@ -1,3 +1,4 @@
+"""
 def fib(K):
     if K == 1:
         return 1
@@ -7,4 +8,14 @@ def fib(K):
         return fib(K-1) + fib(K-2)
     
 print(fib(int(input()))) 
+"""
 
+#2020.12.14
+def Fib(a):
+    if a == 0:
+        return 0
+    if a == 1:
+        return 1
+    return Fib(a-1) + Fib(a-2)
+
+print(Fib(int(input())))
