5328c9d41d9f74d03a4b2b07a5e78ff8835b47e4
Sae_Chan_Oh <dhtocks@naver.com>
2021-07-05 10:33:15 +0900

    Update [1920] 수 찾기.py

--- [1920] 수 찾기.py
+++ [1920] 수 찾기.py
@@ -27,6 +27,7 @@ for k in M:
         print(0)
 """
       
+""""
 #2020.12.11
 input()
 arr = list(map(int, input().split()))
@@ -60,3 +61,37 @@ def Check(target):
 
 for target in arr2:
     print(Check(target))
+"""
+#2021.07.05
+def binary_search(find):
+
+    left = 0
+    right = len(arr) - 1
+    
+    if find < arr[left] or find > arr[right]:
+        return 0
+
+    while left <= right:
+
+        mid = (left + right) // 2
+
+        if arr[mid] == find:
+            return 1
+
+        elif arr[mid] < find:
+            left = mid + 1
+
+        elif arr[mid] > find:
+            right = mid - 1
+
+    return 0
+
+
+input()
+arr = list(map(int, input().split()))
+input()
+finds = list(map(int, input().split()))
+
+arr.sort()
+for find in finds:
+    print(binary_search(find))
