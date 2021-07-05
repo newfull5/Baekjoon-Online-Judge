7f645351f60ed2edbf6c7a703f7785932347c928
Sae_Chan_Oh <dhtocks@naver.com>
2021-07-05 10:51:27 +0900

    Update [10816] 숫자 카드 2.py

--- [10816] 숫자 카드 2.py
+++ [10816] 숫자 카드 2.py
@@ -1,3 +1,4 @@
+"""
 from sys import stdin
 from collections import Counter
 _ = stdin.readline()
@@ -7,3 +8,14 @@ M = stdin.readline().split()
 
 C = Counter(N)
 print(' '.join(f'{C[m]}' if m in C else '0' for m in M))
+"""
+
+#2021.07.05
+from collections import Counter
+
+input()
+arr = Counter(list(map(int, input().split())))
+input()
+finds = list(map(int, input().split()))
+    
+print(*[arr[find] for find in finds])
